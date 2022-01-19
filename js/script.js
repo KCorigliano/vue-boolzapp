6fffc329605b460a3666dc33da3dcab1cebbcbc0
KCorigliano <corigliano98@hotmail.com>
2022-01-19 18:12:57 +0100

    Bonus milestone 5

--- js/script.js
+++ js/script.js
@@ -11,16 +11,19 @@ new Vue({
             date: "10/01/2020 15:30:55",
             text: "Hai portato a spasso il cane?",
             status: "sent",
+            submenu: false,
           },
           {
             date: "10/01/2020 15:50:00",
             text: "Ricordati di dargli da mangiare",
             status: "sent",
+            submenu: false,
           },
           {
             date: "10/01/2020 16:15:22",
             text: "Tutto fatto!",
             status: "received",
+            submenu: false,
           },
         ],
       },
@@ -33,16 +36,19 @@ new Vue({
             date: "20/03/2020 16:30:00",
             text: "Ciao come stai?",
             status: "sent",
+            submenu: false,
           },
           {
             date: "20/03/2020 16:30:55",
             text: "Bene grazie! Stasera ci vediamo?",
             status: "received",
+            submenu: false,
           },
           {
             date: "20/03/2020 16:35:00",
             text: "Mi piacerebbe ma devo andare a fare la spesa.",
             status: "sent",
+            submenu: false,
           },
         ],
       },
@@ -55,16 +61,19 @@ new Vue({
             date: "28/03/2020 10:10:40",
             text: "La Marianna va in campagna",
             status: "received",
+            submenu: false,
           },
           {
             date: "28/03/2020 10:20:10",
             text: "Sicuro di non aver sbagliato chat?",
             status: "sent",
+            submenu: false,
           },
           {
             date: "28/03/2020 16:15:22",
             text: "Ah scusa!",
             status: "received",
+            submenu: false,
           },
         ],
       },
@@ -77,11 +86,13 @@ new Vue({
             date: "10/01/2020 15:30:55",
             text: "Lo sai che ha aperto una nuova pizzeria?",
             status: "sent",
+            submenu: false,
           },
           {
             date: "10/01/2020 15:50:00",
             text: "Si, ma preferirei andare al cinema",
             status: "received",
+            submenu: false,
           },
         ],
       },
@@ -137,5 +148,8 @@ new Vue({
         console.log(filtered);
       }
     },
+    deleteMessage: function(index, currentIndex){
+        this.contacts[currentIndex].messages.splice(index, 1);
+    }
   },
 });
